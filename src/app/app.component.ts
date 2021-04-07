3c24dfdb2201e56b4bca83eb3bc852b29433d494
Jacques Pagels <jacques.pagels@hotmail.com>
2021-04-07 12:41:47 +0100

    Adding styling and basic service call.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetDataService } from './services/get-data/get-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'entia';
+  constructor(
+    private getData: GetDataService
+  ) {}
+
+  ngOnInit() {
+    this.getData.load()
+      .subscribe(resp => {
+        console.log(resp)
+      })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { GetDataService } from './services/get-data/get-data.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ GetDataService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
